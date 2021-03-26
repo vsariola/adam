564340e6ceca5610b6de919cc5c920f9c47262a7
Veikko Sariola <5684185+vsariola@users.noreply.github.com>
2021-03-26 23:35:02 +0200

    Remove post shader as it is never used.

--- intro/main.c
+++ intro/main.c
@@ -1,7 +1,3 @@
-#define POST_PASS    0
-#define USE_MIPMAPS  1
-#define FAIL_KILL true
-#define PID_QUALIFIER const
 #define SYNC_DELAY 16384 // should be power of 2
 
 // minify windows.h
@@ -19,24 +15,11 @@
 #include "glext.h"
 #include <song.h>
 #include <shader.inl>
-#if POST_PASS
-#include <post.inl>
-#endif
 
 #pragma data_seg(".pixelfmt")
 static const PIXELFORMATDESCRIPTOR pfd = {
-#if BREAK_COMPATIBILITY
-	#if POST_PASS
-			0, 0, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA,
-			0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0
-	#else
-			0, 0, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA,
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0
-	#endif
-#else
 	sizeof(pfd), 1, PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, PFD_TYPE_RGBA,
 	32, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 32, 0, 0, PFD_MAIN_PLANE, 0, 0, 0, 0
-#endif
 };
 
 #pragma data_seg(".screensettings")
@@ -82,8 +65,6 @@ static MMTIME MMTime = { TIME_SAMPLES, 0 };
 #pragma data_seg(".pids")
 // static allocation saves a few bytes
 static int pidMain;
-static int pidPost;
-// static HDC hDC;
 
 void entrypoint(void)
 {
@@ -114,17 +95,11 @@ void entrypoint(void)
 
 	// create and compile shader programs
 	pidMain = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &shader_frag);
-
 	CHECK_ERRORS();
-#if POST_PASS
-	pidPost = ((PFNGLCREATESHADERPROGRAMVPROC)wglGetProcAddress("glCreateShaderProgramv"))(GL_FRAGMENT_SHADER, 1, &post_frag);
-#endif	
+
 	waveOutOpen(&hWaveOut, WAVE_MAPPER, &WaveFMT, NULL, 0, CALLBACK_NULL);
 	waveOutPrepareHeader(hWaveOut, &WaveHDR, sizeof(WaveHDR));
 	waveOutWrite(hWaveOut, &WaveHDR, sizeof(WaveHDR));
-
-	CHECK_ERRORS();
-
 
 	do
 	{
@@ -142,38 +117,11 @@ void entrypoint(void)
 
 		waveOutGetPosition(hWaveOut, &MMTime, sizeof(MMTIME));
 		
-
-		//GLint loc = ((PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation"))(pidMain, VAR_SYNCS);
-		//CHECK_ERRORS();
-
-		//GLfloat f[5];
-		//f[0] = (float)MMTime.u.sample / 44100.0f / 60.0f * 144.0f;		
-		//f[1] = fmod(f[0], 1.0f);
-		// add some offset to time to account for the lag in wave out
 		((PFNGLUNIFORM1FVPROC)wglGetProcAddress("glUniform1fv"))(0, 16, &syncBuf[((MMTime.u.sample + SYNC_DELAY) >> 8) * SU_NUMSYNCS]);
 		CHECK_ERRORS();
 	
-
 		glRects(-1, -1, 1, 1);
 		CHECK_ERRORS();
-
-
-		// render "post process" using the opengl backbuffer
-#if POST_PASS
-		glBindTexture(GL_TEXTURE_2D, 1);
-#if USE_MIPMAPS
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, XRES, YRES, 0);
-		((PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap"))(GL_TEXTURE_2D);
-#else
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, 0, 0, XRES, YRES, 0);
-#endif
-		((PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture"))(GL_TEXTURE0);
-		((PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram"))(pidPost);
-		//((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, 0);
-		glRects(-1, -1, 1, 1);
-#endif
 
 		SwapBuffers(hDC);
 
