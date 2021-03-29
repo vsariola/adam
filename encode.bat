bfd082c2497bce444abc26c7af95f9201f165d69
Veikko Sariola <5684185+vsariola@users.noreply.github.com>
2021-03-29 12:04:21 +0300

    Set higher bit rate for audio encoding.
    Lower bit rate resulted in some audible issues during screeches.

--- encode.bat
+++ encode.bat
@@ -1,1 +1,1 @@
-ffmpeg -i output.avi -f f32le -ar 44100 -ac 2 -i song.raw -c:v libx264 -preset slow -crf 17 -c:a aac -movflags +faststart combined.mp4
+ffmpeg -i output.avi -f f32le -ar 44100 -ac 2 -i song.raw -c:v libx264 -preset slow -crf 17 -c:a aac -b:a 256k -movflags +faststart combined.mp4
